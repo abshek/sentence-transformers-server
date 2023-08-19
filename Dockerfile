0b268e2bc3e8af661df13316128e4bf7a9c6ceb9
Abhishek Agarwal <abhishek.agarwal@okiedokiepay.com>
2023-08-19 12:55:12 +0530

    added dockerfile and first draft of the server

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official Python image as the base image
+FROM python:3.8-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Install any necessary dependencies
+RUN pip install flask
+RUN pip install sentence-transformers
+
+# Copy the rest of the application code into the container
+COPY embedding_server.py .
+
+# Expose the desired port
+EXPOSE 8081
+
+# Command to run the Python script
+CMD ["python", "embedding_server.py"]
---
+++ embedding_server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from sentence_transformers import SentenceTransformer
+
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')  # You can use any other model you prefer
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+@app.route('/transform', methods=['POST'])
+def transform_sentences():
+    try:
+        data = request.json
+        sentences = data['sentences']
+        
+        if not sentences or not isinstance(sentences, list):
+            return jsonify({'error': 'Invalid input format'}), 400
+        
+        embeddings = model.encode(sentences)
+        return jsonify({'embeddings': embeddings.tolist()}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
