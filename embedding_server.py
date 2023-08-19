3fed1c338ac4a99ed6e6e50476d9fe7b7fb16f33
Abhishek Agarwal <abhishek.agarwal@okiedokiepay.com>
2023-08-19 13:28:17 +0530

    rectified embeddings output to http request; normalizing the embedding

--- embedding_server.py
+++ embedding_server.py
@@ -19,7 +19,7 @@ def transform_sentences():
         if not sentences or not isinstance(sentences, list):
             return jsonify({'error': 'Invalid input format'}), 400
         
-        embeddings = model.encode(sentences, convert_to_tensor=True)
+        embeddings = model.encode(sentences)
         return jsonify({'model':model_name, 'embeddings': embeddings.tolist()}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
